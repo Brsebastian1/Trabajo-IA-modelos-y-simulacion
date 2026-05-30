96923eb7786205d8579574abdd7ac4638bb1e5fc
Brsebastian1 <159483437+Brsebastian1@users.noreply.github.com>
2026-05-30 07:45:08 -0500

    Update question-0001-usecase-generator.py

--- myquestions/question-0001-usecase-generator.py
+++ myquestions/question-0001-usecase-generator.py
@@ -2,29 +2,6 @@ import numpy as np
 import pandas as pd
 
 
-def eliminar_multicolinealidad(df, threshold):
-    # Seleccionar columnas numéricas
-    df_numeric = df.select_dtypes(include=[np.number])
-
-    # Calcular matriz de correlación absoluta
-    corr_matrix = df_numeric.corr().abs()
-
-    # Tomar solo la parte superior de la matriz
-    upper_triangle = corr_matrix.where(
-        np.triu(np.ones(corr_matrix.shape), k=1).astype(bool)
-    )
-
-    # Identificar columnas a eliminar
-    columnas_eliminadas = [
-        column for column in upper_triangle.columns
-        if any(upper_triangle[column] > threshold)
-    ]
-
-    # Eliminar columnas
-    df_filtrado = df_numeric.drop(columns=columnas_eliminadas)
-
-    return df_filtrado, columnas_eliminadas
-
 
 def generar_caso_de_uso_eliminar_multicolinealidad():
     np.random.seed()
@@ -64,6 +41,29 @@ def generar_caso_de_uso_eliminar_multicolinealidad():
     output_data = (df_filtrado, columnas_eliminadas)
 
     return input_data, output_data
+
+def eliminar_multicolinealidad(df, threshold):
+    # Seleccionar columnas numéricas
+    df_numeric = df.select_dtypes(include=[np.number])
+
+    # Calcular matriz de correlación absoluta
+    corr_matrix = df_numeric.corr().abs()
+
+    # Tomar solo la parte superior de la matriz
+    upper_triangle = corr_matrix.where(
+        np.triu(np.ones(corr_matrix.shape), k=1).astype(bool)
+    )
+
+    # Identificar columnas a eliminar
+    columnas_eliminadas = [
+        column for column in upper_triangle.columns
+        if any(upper_triangle[column] > threshold)
+    ]
+
+    # Eliminar columnas
+    df_filtrado = df_numeric.drop(columns=columnas_eliminadas)
+
+    return df_filtrado, columnas_eliminadas
 
 
 if __name__ == "__main__":
