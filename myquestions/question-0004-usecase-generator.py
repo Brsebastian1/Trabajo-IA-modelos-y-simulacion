6888a1117e7c3a1ef5d5cf58618cd14f692cf40a
Brsebastian1 <159483437+Brsebastian1@users.noreply.github.com>
2026-05-30 07:44:07 -0500

    Update question-0004-usecase-generator.py

--- myquestions/question-0004-usecase-generator.py
+++ myquestions/question-0004-usecase-generator.py
@@ -3,23 +3,6 @@ import pandas as pd
 from sklearn.decomposition import NMF
 
 
-def extraer_patrones_consumo(df, n_components):
-    X = df.to_numpy()
-
-    modelo = NMF(
-        n_components=n_components,
-        init="random",
-        random_state=42,
-        max_iter=500
-    )
-
-    W = modelo.fit_transform(X)
-    H = modelo.components_
-
-    X_reconstruida = np.dot(W, H)
-    rmse = float(np.sqrt(np.mean((X - X_reconstruida) ** 2)))
-
-    return W, H, rmse
 
 
 def generar_caso_de_uso_extraer_patrones_consumo():
@@ -47,6 +30,25 @@ def generar_caso_de_uso_extraer_patrones_consumo():
     output_data = extraer_patrones_consumo(df, n_components)
 
     return input_data, output_data
+
+def extraer_patrones_consumo(df, n_components):
+    X = df.to_numpy()
+
+    modelo = NMF(
+        n_components=n_components,
+        init="random",
+        random_state=42,
+        max_iter=500
+    )
+
+    W = modelo.fit_transform(X)
+    H = modelo.components_
+
+    X_reconstruida = np.dot(W, H)
+    rmse = float(np.sqrt(np.mean((X - X_reconstruida) ** 2)))
+
+    return W, H, rmse
+
 
 
 if __name__ == "__main__":
